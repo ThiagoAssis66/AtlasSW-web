6abbe0cea2676faf0b18e75ad7b3de459d5236c4
Thiago Assis <th.batistaassis66@gmail.com>
2024-11-03 13:45:22 -0300

    atualizando server email

--- server/email-api/server.js
+++ server/email-api/server.js
@@ -1,58 +1,4 @@
-const express = require('express');
-const nodemailer = require('nodemailer');
-const cors = require('cors');
-require('dotenv').config();
 
-const app = express();
-const PORT = process.env.PORT || 5000;
-
-// Configure CORS para permitir apenas o domínio do GitHub Pages
-const corsOptions = {
-  origin: 'https://github.com/ThiagoAssis66/AtlasSW-web', 
-  methods: ['POST'],
-  allowedHeaders: ['Content-Type'],
-};
-
-app.use(cors(corsOptions));
-app.use(express.json());
-
-// Configuração do Nodemailer com as credenciais do Gmail
-const transporter = nodemailer.createTransport({
-  service: 'gmail',
-  auth: {
-    user: process.env.EMAIL_USER,
-    pass: process.env.EMAIL_PASS,
-  },
-});
-
-// Rota para enviar o email
-app.post('/send', (req, res) => {
-  const { name, email, message } = req.body;
-
-  const mailOptions = {
-    from: email,
-    to: process.env.EMAIL_USER,
-    subject: `Contato de ${name}`,
-    text: `Nome: ${name}\nEmail: ${email}\nMensagem: ${message}`,
-  };
-
-  transporter.sendMail(mailOptions, (error, info) => {
-    if (error) {
-      console.log(error);
-      return res.status(500).send('Erro ao enviar email');
-    }
-    res.status(200).send('Email enviado com sucesso');
-  });
-});
-
-app.listen(PORT, () => {
-  console.log(`Servidor rodando na porta ${PORT}`);
-});
-
-
-
-
-/*
  
 
 //SERVIDOR LOCAL
@@ -101,4 +47,3 @@ app.post('/send', (req, res) => {
 app.listen(PORT, () => {
   console.log(`Servidor rodando na porta ${PORT}`);
 });
-*/
